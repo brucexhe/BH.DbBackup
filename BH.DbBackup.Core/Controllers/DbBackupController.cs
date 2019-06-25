e9d5ed6b0a4e26c266260faed82a542e7cfcca1e
Bruce He <Bruce_X_He@manulife.com>
2019-06-25 16:40:03 +0800

    init commit

---
+++ BH.DbBackup.Core/Controllers/DbBackupController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+
+namespace BH.DbBackup.Core
+{
+    public class DbBackupController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return Content(ResourceUtil.GetResource("static/index.html"), "text/html");
+            //return View();// Content("db backup index");
+        }
+
+        public IActionResult GetResource(string fileName)
+        {
+            return Content(ResourceUtil.GetResource(fileName));
+        }
+
+        public JsonResult GetDbList()
+        {
+            List<String> dbList = new List<string>();
+            dbList.Add("mysql");
+            return Json(dbList);
+        }
+    }
+}
---
+++ BH.DbBackup.Core/Utils/ResourceUtil.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace BH.DbBackup.Core
+{
+    public class ResourceUtil
+    {
+        static EmbeddedFileProvider fileProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+        public static String GetResource(string key)
+        {
+            var fi = fileProvider.GetFileInfo(key);
+            byte[] bytes = new byte[fi.Length];
+            using (var stream = fi.CreateReadStream())
+            {
+                stream.Read(bytes, 0, Convert.ToInt32(fi.Length));
+            }
+            var result = Encoding.UTF8.GetString(bytes);
+
+            return result;
+        }
+    }
+}
